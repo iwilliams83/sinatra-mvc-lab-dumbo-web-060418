3badf30dac39a78151fc3326eb6108474807f6a3
iwilliams83 <indirajamurath@gmail.com>
2018-06-27 13:44:04 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,52 +1,37 @@
 class PigLatinizer
   # attr_accessor :word
 
-  def count_of_words(str)
-    arr = str.split(" ")
-    arr.count
+  def piglatinize(input_str)
+  x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  puts x
+  x
+end
+
+private
+
+def consonant?(char)
+  !char.match(/[aAeEiIoOuU]/)
+end
+
+def piglatinize_word(word)
+  # word starts with vowel
+  if !consonant?(word[0])
+    word = word + "w"
+  # word starts with 3 consonants
+  elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+    word = word.slice(3..-1) + word.slice(0,3)
+  # word starts with 2 consonants
+  elsif consonant?(word[0]) && consonant?(word[1])
+    word = word.slice(2..-1) + word.slice(0,2)
+  # word starts with 1 consonant
+  else
+    word = word.slice(1..-1) + word.slice(0)
   end
+  word << "ay"
+end
 
-  def translate(word)
-    alpha = ('a'..'z').to_a
-    alpha << ('A'..'Z').to_a
-    alpha.flatten
-    vowels = %w[a e i o u A E I O U]
-    consonants = alpha - vowels
-
-    if word == "I" || word == 'i'
-      word + 'way'
-    elsif vowels.include?(word[0])
-      word + 'way'
-    elsif consonants.include?(word[0]) && consonants.include?(word[1])
-      word[2..-1] + word[0..1] + 'ay'
-    elsif consonants.include?(word[0])
-      word[1..-1] + word[0] + 'ay'
-    else
-      word # return unchanged
-    end
-  end
-
-  def piglatinize(str)
-    alpha = ('a'..'z').to_a
-    alpha << ('A'..'Z').to_a
-    alpha.flatten
-    vowels = %w[a e i o u A E I O U]
-
-    new_sentence = []
-
-    if count_of_words(str) == 1
-      str = translate(str)
-      new_sentence << str
-    elsif count_of_words(str) > 1
-      arr = str.split(" ")
-      arr.each do |word|
-        word = translate(word)
-        new_sentence << word
-      end
-    end
-
-    new_sentence.join(" ")
-
-  end #def end
+def piglatinize_sentence(sentence)
+  sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+end
 
 end #class end
